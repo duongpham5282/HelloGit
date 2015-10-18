0c44506e37c677aabc190c68a6235504b612b6d6
Duong <ptbduong@yahoo.com>
2015-10-18 18:40:47 +0700

    1

--- HelloGit/src/com/cboe/Test.java
+++ HelloGit/src/com/cboe/Test.java
@@ -6,23 +6,7 @@ public class Test {
 	{
 		System.out.println("test only");
 	}
-	private void t1()
-	{
-		System.out.println("test only1");
-	}
-	private void t1()
-	{
-		int i = 120;
-	}
-	
-	
-	
-	
-	
-	private void t1()
-	{
-		int i = 120;
-	}
+
 	
 	public int getEmmID()
 	{
