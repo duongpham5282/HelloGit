2a2226d49c5c7a614bb89f2beaa957f35012bd48
Duong <ptbduong@yahoo.com>
2015-10-18 18:51:56 +0700

    Revert "1"
    This reverts commit 0c44506e37c677aabc190c68a6235504b612b6d6.

--- HelloGit/src/com/cboe/Test.java
+++ HelloGit/src/com/cboe/Test.java
@@ -6,7 +6,23 @@ public class Test {
 	{
 		System.out.println("test only");
 	}
-
+	private void t1()
+	{
+		System.out.println("test only1");
+	}
+	private void t1()
+	{
+		int i = 120;
+	}
+	
+	
+	
+	
+	
+	private void t1()
+	{
+		int i = 120;
+	}
 	
 	public int getEmmID()
 	{
